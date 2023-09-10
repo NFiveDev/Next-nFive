6549d28efc4acc00f57b4cb8ad73683b8f8eb585
NFiveDev <nmhjort7@gmail.com>
2023-09-10 20:15:45 +0200

    dark mode switch

--- src/app/providers.tsx
+++ src/app/providers.tsx
@@ -1,11 +1,32 @@
 'use client'
 
 import {NextUIProvider} from '@nextui-org/react';
+import {ThemeProvider as NextThemesProvider} from "next-themes";
 
 export function Providers({children}: {children: React.ReactNode}) {
   return (
     <NextUIProvider>
+      <NextThemesProvider attribute="class" defaultTheme="light">
       {children}
+      </NextThemesProvider>
     </NextUIProvider>
   )
 }
+
+/*
+// app/providers.tsx
+"use client";
+
+import {NextUIProvider} from '@nextui-org/react'
+import {ThemeProvider as NextThemesProvider} from "next-themes";
+
+export function Providers({children}: { children: React.ReactNode }) {
+  return (
+    <NextUIProvider>
+      <NextThemesProvider attribute="class" defaultTheme="dark">
+        {children}
+      </NextThemesProvider>
+    </NextUIProvider>
+  )
+}
+*/
--- src/components/navbar/themeSwitch.tsx
+++ src/components/navbar/themeSwitch.tsx
@@ -1,13 +1,38 @@
-import {Switch} from "@nextui-org/react";
+"use client";
 
-function ThemeSwitch(){
+import { Switch } from "@nextui-org/react";
+import { useState, useEffect } from "react";
+import { HiSun, HiMoon } from "react-icons/hi2";
+import {useTheme} from "next-themes";
+
+function ThemeSwitch() {
+  const [mounted, setMounted] = useState(false);
+  const { theme, setTheme } = useTheme();
+
+
+  useEffect(() => {
+    setMounted(true);
+  }, []);
+
+  const handleOnChange = () => {
+    const nextTheme = theme === 'light' ? 'dark' : 'light';
+    setTheme(nextTheme)
+  }
+
+  if (!mounted) return null;
+
   return (
-    <>
-      <div className='flex flex-row gap-1'>
-        
-      </div>
-    </>
-  )
+    <div className="flex items-center">
+      <Switch
+        defaultSelected
+        size="sm"
+        color="success"
+        startContent={<HiSun />}
+        endContent={<HiMoon />}
+        onChange={handleOnChange}
+      />
+    </div>
+  );
 }
 
 export default ThemeSwitch;
