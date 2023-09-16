046977f99d4c95f5b561fdd093dbe063fd92eae3
NFiveDev <nmhjort7@gmail.com>
2023-09-16 02:36:55 +0200

    footer done

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -1,7 +1,28 @@
+import Link from 'next/link';
+import { FaFacebook, FaGithub, FaTwitch } from 'react-icons/fa';
+
 function Footer() {
   return (
-    <footer className='w-full'>
-      <div className='max-w-7xl mx-auto px-6'>footer</div>
+    <footer className='w-full py-3'>
+      <div className='max-w-7xl mx-auto px-6 flex flex-row-reverse'>
+        <div className='flex flex-row gap-4 me-2'>
+          <div className=''>
+            <Link href=''>
+              <FaTwitch className='w-6 h-6 text-slate-500 hover:text-slate-700' />
+            </Link>
+          </div>
+          <div>
+            <Link href=''>
+              <FaFacebook className='w-6 h-6 text-slate-500 hover:text-slate-700' />
+            </Link>
+          </div>
+          <div>
+            <Link href=''>
+              <FaGithub className='w-6 h-6 text-slate-500 hover:text-slate-700' />
+            </Link>
+          </div>
+        </div>
+      </div>
     </footer>
   );
 }
