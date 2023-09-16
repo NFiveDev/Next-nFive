127c2463d7d7033656a2c0aaefac7d501700494b
NFiveDev <nmhjort7@gmail.com>
2023-09-16 02:23:59 +0200

    feature section almost done

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,30 +1,92 @@
-import { ButtonDefault } from '@/components/buttons'
-import {HiPlayCircle, HiAcademicCap} from 'react-icons/hi2'
+import { ButtonDefault } from '@/components/buttons';
+import { HiPlayCircle, HiAcademicCap } from 'react-icons/hi2';
 import { FeatureCard } from '@/components/cards';
+
+
+const featureSectionConfig = [
+  {
+    icon: <HiAcademicCap className='w-6 h-6 text-emerald-700'/>,
+    title: 'Project Based Learning',
+    link: {
+      displayText: 'Learn more',
+      ref: '#'
+    },
+    bodyText: 'Plan it, create it, launch it. Collaborate seamlessly with all the organization and hit your marketing goals every month with our marketing plan.',
+  },
+  {
+    icon: <HiAcademicCap className='w-6 h-6 text-emerald-700'/>,
+    title: 'Project Based Learning',
+    link: {
+      displayText: 'Learn more',
+      ref: '#'
+    },
+    bodyText: 'Plan it, create it, launch it. Collaborate seamlessly with all the organization and hit your marketing goals every month with our marketing plan.',
+  },
+  {
+    icon: <HiAcademicCap className='w-6 h-6 text-emerald-700'/>,
+    title: 'Project Based Learning',
+    link: {
+      displayText: 'Learn more',
+      ref: '#'
+    },
+    bodyText: 'Plan it, create it, launch it. Collaborate seamlessly with all the organization and hit your marketing goals every month with our marketing plan.',
+  }
+]
+
+
 
 export default function Home() {
   return (
     <main className=''>
       <section className='flex flex-col items-center text-center pt-32 pb-28'>
-        <h1 className='text-slate-800 dark:text-slate-400 text-6xl font-normal py-2'>Future of Learning in <span className='text-emerald-500'>IT</span> and <span className='text-emerald-500'>Programming</span></h1>
-        <p className='text-cyan-800 text-lg font-medium py-2 px-36 dark:text-cyan-400'> 
-          Discover the Future of Learning through Interactive Videos and Project-Based Courses. Join Our Inclusive Community, Where Aspiration Meets Expertise, Welcoming Learners of All Levels.
+        <h1 className='text-slate-800 dark:text-slate-400 text-6xl font-normal py-2'>
+          Future of Learning in <span className='text-emerald-500'>IT</span> and{' '}
+          <span className='text-emerald-500'>Programming</span>
+        </h1>
+        <p className='text-cyan-800 text-lg font-medium py-2 px-36 dark:text-cyan-400'>
+          Discover the Future of Learning through Interactive Videos and
+          Project-Based Courses. Join Our Inclusive Community, Where Aspiration
+          Meets Expertise, Welcoming Learners of All Levels.
         </p>
         <div className='gap-4 flex py-5'>
-          <ButtonDefault color='primary' size='lg'>Explore</ButtonDefault>
-          <ButtonDefault color='secondary' variant='bordered' size='lg' className='gap-2'>Intro<HiPlayCircle className='w-5 h-5 p-0 m-0' /></ButtonDefault>
+          <ButtonDefault color='primary' size='lg'>
+            Explore
+          </ButtonDefault>
+          <ButtonDefault
+            color='secondary'
+            variant='bordered'
+            size='lg'
+            className='gap-2'
+          >
+            Intro
+            <HiPlayCircle className='w-5 h-5 p-0 m-0' />
+          </ButtonDefault>
         </div>
       </section>
-
 
       <section className=''>
         <div className='grid grid-cols-3 gap-5 px-10'>
-          <div>
-            <FeatureCard icon={<HiAcademicCap classname='w-8 h-8'/>} bodyContent='' link={{displayText: 'Learn more', ref: '#'}}></FeatureCard>
-          </div>
 
+          {
+            featureSectionConfig.map(function(feature, index){
+              return (
+                <div key={index}>
+                <FeatureCard
+                  icon={feature.icon}
+                  title={feature.title}
+                  link={feature.link}
+                >
+                  <span className='text-sky-700 font-base text-base'>
+                    {feature.bodyText}
+                  </span>
+                </FeatureCard>
+              </div>
+              )
+            })
+          }
+         
         </div>
       </section>
     </main>
-  )
+  );
 }
--- src/components/cards.tsx
+++ src/components/cards.tsx
@@ -1,16 +1,17 @@
 'use client'
 
-import { Card, CardHeader, CardBody, CardProps, CardFooter, Link } from '@nextui-org/react'
+import { Card, CardHeader, CardBody, CardProps, CardFooter, Link, Divider } from '@nextui-org/react'
 import { IconType } from 'react-icons'
 import { HiArrowRight } from 'react-icons/hi2'
 
 type CustomCardProps = {
   icon?: React.ReactNode,
-  bodyContent?: string,
+  children?: React.ReactNode,
   link?: {
     ref: string,
     displayText: string
   }
+  title?: string
 }
 
 export function FeatureCard(props: CustomCardProps & Partial<CardProps>) {
@@ -20,13 +21,16 @@ export function FeatureCard(props: CustomCardProps & Partial<CardProps>) {
         <div className='p-2 bg-green-100 rounded-full'>
           {props.icon && props.icon}
         </div>
+        <span className='ps-2 text-slate-800 font-bold '>{props.title}</span>
       </CardHeader>
+      <Divider />
       <CardBody>
-        {props.bodyContent && props.bodyContent}
-      </CardBody>
-      <CardFooter>
+        {props.children}
+        <div className='pt-2'>
         {props.link && <Link className='text-blue-600 font-semibold' href={props.link.ref}>{props.link.displayText} <div className='px-1'><HiArrowRight /></div></Link> }
-      </CardFooter>
+        </div>
+      </CardBody>
+      
     </Card>
   )
 }
